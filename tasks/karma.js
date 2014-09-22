b4722cf49efebbc8abc8af6b28a3d8c7d704d49d
evanweible-wf <evan.weible@webfilings.com>
2014-09-22 12:55:07 -0500

    Use files option from karma.conf.js when running gulp-karma to allow configuration

--- tasks/karma.js
+++ tasks/karma.js
@@ -6,7 +6,7 @@ module.exports = function(gulp, options, subtasks) {
     var gutil = require('gulp-util');
     var karma = require('gulp-karma');
     var path = require('path');
-    
+
     var taskname = 'karma';
 
     gulp.desc(taskname, 'Start the karma test runner for any test run');
@@ -30,8 +30,12 @@ module.exports = function(gulp, options, subtasks) {
             }
         }
 
-        var polyfill = path.resolve(cwd, "node_modules/wGulp/src/function-bind-polyfill.js");
-        var files = [polyfill];
+        // get files from karma config
+        var karmaShim = {set: function(config) { this.config = config; }};
+        var karmaConf = require(path.resolve(cwd, 'node_modules/wGulp/karma.conf.js'));
+        karmaConf(karmaShim);
+        var files = karmaShim.config.files;
+
         return gulp.src(files)
             .pipe(karma({
                 configFile: options.karma,
