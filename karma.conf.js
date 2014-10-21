7ea9b82895b1f0ed2e4272175d6813e00e213304
Trent Grover <trent.grover@webfilings.com>
2014-10-21 10:02:40 -0500

    serve up all the files to karma

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(karma) {
         frameworks: ['jspm', 'jasmine'],
         jspm: {
             loadFiles: ['build/test/**/*.js'],
-            serveFiles: ['build/src/**/*.js', 'build/src/**/*.map', 'build/test/**/*.map', 'src/**/*.ts', 'test/**/*.ts']
+            serveFiles: ['build/**', 'src/**', 'test/**']
         },
         reporters: ['progress', 'junit', 'html'],
         junitReporter: {
